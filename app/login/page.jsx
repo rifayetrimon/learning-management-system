1fdb77c8871e5494fb1e8013677113665b406e46
rifayet <rifayetrimon88@gmail.com>
2025-07-07 18:29:28 +0800

    add login page form

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,9 @@
+import LoginForm from "@/components/auth/login";
+
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100"></div>
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <LoginForm />
+    </div>
   );
 }
