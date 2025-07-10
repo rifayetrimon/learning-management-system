275bc6e69af82aea29c81e05643b2639181b600f
rifayet <rifayetrimon88@gmail.com>
2025-07-10 18:28:23 +0800

    fix expected size of screen

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,26 @@
+"use client";
+
 import "./globals.css";
 import BottomNav from "@/components/layout/BottomNav";
-import { AllMenuDrawer } from "../components/layout/AllMenuDrawer";
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+  const isLoginPage = pathname === "/login";
+
   return (
     <html lang="en">
       <body>
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
-          <div className="w-full max-w-2xl min-h-[80vh] bg-neutral-50 rounded-2xl shadow-lg flex flex-col relative overflow-hidden">
-            <div className="flex-1 p-6 pb-20 overflow-y-auto">{children}</div>
-            <BottomNav />
+          <div className="w-full max-w-2xl min-h-[90vh] bg-neutral-50 rounded-2xl shadow-lg flex flex-col relative overflow-hidden">
+            <div
+              className={`flex-1 p-6 pb-20 overflow-y-auto ${
+                isLoginPage ? "flex items-center justify-center" : ""
+              }`}
+            >
+              {children}
+            </div>
+            {!isLoginPage && <BottomNav />}
           </div>
         </div>
       </body>
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,8 +2,7 @@ import LoginForm from "@/components/auth/login";
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <LoginForm />
-    </div>
+
   );
 }
